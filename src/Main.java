3cb85b9b76012cff22756178ac2642723dcfa491
Anamooz <145793103+Anamooz@users.noreply.github.com>
2023-09-22 23:24:24 -0700

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("hopefully this can also run in masters and not main");
+
         System.out.println("Hello world!");
     }
 }
