0adfdf05c823387e0966f41b208a58b3b136df0f
Anamooz <145793103+Anamooz@users.noreply.github.com>
2023-09-22 23:29:53 -0700

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("hopefully this can also run in masters and not main");
         System.out.println("lets try again");
         System.out.println("Hello world!");
+        System.out.println("lets try pushing into master");
     }
 }
